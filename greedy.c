707a2f0b45c37ec41ed2df49ae8f801f911808db
Inderdeep23 <inderdeep235@gmail.com>
2012-07-04 19:03:59 +0530

    Update master

--- greedy.c
+++ greedy.c
@@ -4,11 +4,42 @@
 int main()
 {
 
-float change;
-int number;
+double change;
+int coins , amt = 0;
 
 printf("How much change do you owe?");
-scanf("%f", &change);
+scanf("%lf", &change);
+
+coins = (int)(change/25);
+change =  change - (coins*25);
+amt =  amt + coins;
+
+coins = (int)(change/10);
+change =  change - (coins*10);
+amt =  amt + coins;
+
+coins = (int)(change/5);
+change =  change - (coins*5);
+amt =  amt + coins;
+
+coins = (int)(change/1);
+change =  change - (coins*1);
+amt =  amt + coins;
+
+printf("total no of coins= %d", amt);
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
