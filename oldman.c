f03af63c07a868e23ff1353ecf9bf41597fc5e07
Inderdeep Singh <inderdeep235@gmail.com>
2012-07-10 18:36:25 +0530

    newfile

--- oldman.c
+++ oldman.c
@@ -3,11 +3,9 @@
 #include<cs50.h>
 
 
-
 int main()
 
 {
-
 
 int index;
 char choice;
@@ -16,37 +14,32 @@ string numbers[] = {"ONE","TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT
 string words[] = {"ON MY THUMB","ON MY SHOE","ON MY KNEE", "ON MY DOOR", "ON MY HIVE", "ON MY STICKS", "UP IN HEAVEN", "ON MY GATE", "ON MY SPINE","ONCE AGAIN"};
 
 
-
 printf("Do you wanna hear a song?\n");
 scanf("%c", &choice);
 
-if((choice == 'y')  || (choice == 'Y'))   // prompt the user for input 
+if((choice == 'y')  || (choice == 'Y'))   // prompt the user for choice  
 {
 
-//initializing a for loop
+/*****initializing a for loop*****/
 
 for(index = 0; index<10; index++)
 
 {
 
-printf("This old man, he played %s\nHe played knick-knacK %s\nKnick-knack paddywhackive your dog a bone\nThis old man came rolling home\n", numbers[index], words[index]);
+printf("\nThis old man, he played %s\nHe played knick-knacK %s\nKnick-knack paddywhackive your dog a bone\nThis old man came rolling home\n", numbers[index], words[index]);
 
 }
 
 }
-
 
 else
 {
 if((choice == 'n') || (choice=='N'))
 {
 
-printf("sorry to hear that?");
-
-
+printf("\nsorry to hear that\n?");
 
 }
-
 
 }
 
